aedaa1250dfad67a0166146514a71ae03281f5b3
Jack Chan <JackChanHW@connect.hku.hk>
2022-12-02 12:39:27 +0000

    Create node version

---
+++ node/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import morgan from "morgan"
+
+const app = express()
+
+app.use(morgan("dev"))
+
+const port = 3000
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
